8297f0b4f05f4b267adf590ef9ee19b8778fd845
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-13 22:01:44 +0000

    Add src/functions/api.py

---
+++ src/functions/api.py
@@ -0,0 +1,35 @@
+"""Module api.py"""
+import sys
+
+import yaml
+import requests
+
+
+class API:
+    """
+    Class API
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, url: str) -> dict:
+        """
+
+        :param url: An online data source URL (Uniform Resource Locator)
+        :return:
+        """
+
+        try:
+            response = requests.get(url=url, timeout=600)
+            response.raise_for_status()
+        except requests.exceptions.Timeout as err:
+            raise err from err
+        except Exception as err:
+            raise err from err
+
+        if response.status_code == 200:
+            content = response.content.decode(encoding='utf-8')
+            return yaml.safe_load(content)
+
+        sys.exit(response.status_code)
