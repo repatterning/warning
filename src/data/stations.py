d57500b70a06b071ccfcc38e3db1ec18cb92fee5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-03 21:11:57 +0000

    Update src/data/stations.py

--- src/data/stations.py
+++ src/data/stations.py
@@ -7,12 +7,18 @@ import src.functions.streams
 
 
 class Stations:
+    """
+    <b>Notes</b><br>
+    ------<br>
+
+    The stations.<br>
+    """
 
     def __init__(self):
 
         self.__uri = (
             'https://timeseries.sepa.org.uk/KiWIS/KiWIS?service=kisters&type=queryServices&datasource=0'
-            '&request=getstationlist&returnfields=station_no,station_name,stationparameter_name,'
+            '&request=getstationlist&returnfields=station_id,station_no,station_name,stationparameter_name,'
             'stationparameter_no,catchment_id,catchment_no,catchment_name,station_latitude,station_longitude,'
             'station_carteasting,station_cartnorthing,river_id,river_name,ca_sta&'
             'ca_sta_returnfields=CATCHMENT_SIZE,GAUGE_DATUM,GROUND_DATUM,GWREF_DATUM&object_type=General&format=csv')
@@ -28,10 +34,13 @@ class Stations:
 
         text = txa.TextAttributes(uri=self.__uri, header=0, sep=';')
         data = self.__streams.api(text=text)
+        logging.info('STATIONS:\n%s', data.head())
         data.info()
+
+        excerpt = data.loc[data['station_id'] == 36870, :]
+        logging.info('STATIONS: %s',
+                     excerpt[['station_id','station_no','station_name', 'stationparameter_name', 'river_id', 'river_name']])
 
         frequency = data[['catchment_id', 'catchment_no', 'catchment_name']].groupby(
             by=['catchment_id', 'catchment_no', 'catchment_name']).value_counts()
-        logging.info(frequency)
-
-        logging.info(data.loc[data['catchment_no'] == 83, :])
+        logging.info('FREQUENCY:\n%s', frequency)
