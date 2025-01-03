7d4193847762ada16878834e5dbeb583ee7a1b1c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-03 12:58:28 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 """Module main.py"""
-import argparse
 import logging
 import os
 import sys
-
-import boto3
-import torch
 
 
 def main():
@@ -16,20 +12,7 @@ def main():
     """
 
     logger: logging.Logger = logging.getLogger(__name__)
-
-    # Set up
-    setup: bool = src.setup.Setup(service=service, s3_parameters=s3_parameters, architecture=architecture).exc()
-    if not setup:
-        src.functions.cache.Cache().exc()
-        sys.exit('No Executions')
-
-    # Device Selection: Setting a graphics processing unit as the default device
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    logger.info('Device: %s', device)
-
-    # Hence
-    src.algorithms.interface.Interface().exc(architecture=architecture)
-    src.transfer.interface.Interface(service=service, s3_parameters=s3_parameters).exc()
+    logger.info('SANDBOX')
 
     # Delete Cache Points
     src.functions.cache.Cache().exc()
@@ -53,26 +36,5 @@ if __name__ == '__main__':
     # Modules
     import src.algorithms.interface
     import src.functions.cache
-    import src.functions.expecting
-    import src.functions.service
-    import src.s3.s3_parameters
-    import src.setup
-    import src.transfer.interface
-
-    expecting = src.functions.expecting.Expecting()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--architecture', type=expecting.architecture,
-                        help='The name of the architecture in focus.')
-    args = parser.parse_args()
-
-    # Default architecture?
-    architecture = 'distil' if args.architecture is None else args.architecture
-
-    # S3 S3Parameters, Service Instance
-    connector = boto3.session.Session()
-    s3_parameters = src.s3.s3_parameters.S3Parameters(connector=connector).exc()
-    service = src.functions.service.Service(connector=connector, region_name=s3_parameters.region_name).exc()
-
-
 
     main()
