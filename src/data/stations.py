253a3c039557811c40a5b27a24a410f1fc269529
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-03 14:34:11 +0000

    Add src/data/stations.py

---
+++ src/data/stations.py
@@ -0,0 +1,28 @@
+
+import logging
+
+import src.elements.text_attributes as txa
+import src.functions.objects
+import src.functions.streams
+
+
+class Stations:
+
+    def __init__(self):
+
+        self.__uri = (
+            'https://timeseries.sepa.org.uk/KiWIS/KiWIS?service=kisters&type=queryServices&datasource=0'
+            '&request=getstationlist&returnfields=station_no,station_name,stationparameter_name,'
+            'stationparameter_no,catchment_id,catchment_no,catchment_name,station_latitude,station_longitude,'
+            'station_carteasting,station_cartnorthing,river_id,river_name,ca_sta&'
+            'ca_sta_returnfields=CATCHMENT_SIZE,GAUGE_DATUM,GROUND_DATUM,GWREF_DATUM&object_type=General&format=csv')
+
+        self.__streams = src.functions.streams.Streams()
+
+    def exc(self):
+
+        text = txa.TextAttributes(uri=self.__uri, header=0, sep=';')
+
+        data = self.__streams.api(text=text)
+
+        logging.info(data)
