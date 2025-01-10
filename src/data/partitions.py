91e2bf8aeb2231acfc2c932559a302de26b84125
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-10 19:46:24 +0000

    Update src/data/partitions.py: In Progress

--- src/data/partitions.py
+++ src/data/partitions.py
@@ -1,13 +1,27 @@
 import logging
 import pandas as pd
+import numpy as np
+import datetime
 
 import config
+
+import src.elements.partitions as prt
 
 
 class Partitions:
 
-    def __init__(self):
+    def __init__(self, pilot: pd.DataFrame):
+        """
+        'station_id', 'catchment_id', 'catchment_name', 'ts_id', 'ts_name', 'from', 'to',
+        'stationparameter_no', 'parametertype_id', 'station_latitude', 'station_longitude', 'river_id',
+        'CATCHMENT_SIZE', 'GAUGE_DATUM'
 
+        :param pilot:
+        """
+
+        self.__pilot = pilot
+
+        # Configurations
         self.__configurations = config.Config()
 
         # Logging
@@ -16,8 +30,23 @@ class Partitions:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
+    def __matrix(self, datestr: np.datetime64):
+
+        data = self.__pilot.copy()
+        data = data.assign(period = datestr)
+
+        self.__logger.info(data.to_dict(orient='index'))
+
+
     def exc(self):
 
-        dates = pd.date_range(start=self.__configurations.starting, end=self.__configurations.at_least, freq='MS')
+        dates = pd.date_range(
+            start=self.__configurations.starting, end=self.__configurations.at_least, freq='MS').to_frame(
+            index=False, name='date')
+        values: pd.Series = dates['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+        self.__logger.info(type(values))
 
-        self.__logger.info(dates)
+        for datestr in values.values:
+
+            self.__logger.info(type(datestr))
+            self.__logger.info(str(datestr))
