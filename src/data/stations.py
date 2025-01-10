28c08c64bc661392a8a19849fdd01319b01bac42
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-10 17:13:33 +0000

    Update src/data/stations.py

--- src/data/stations.py
+++ src/data/stations.py
@@ -14,15 +14,19 @@ class Stations:
     """
 
     def __init__(self):
+        """
+        Constructor
+        """
 
+        self.__streams = src.functions.streams.Streams()
+
+        # The uniform resource locator for the stations vis-à-vis <Level>
         self.__uri = (
             'https://timeseries.sepa.org.uk/KiWIS/KiWIS?service=kisters&type=queryServices&datasource=0'
-            '&request=getstationlist&returnfields=station_id,station_no,station_name,stationparameter_no,'
+            '&request=getstationlist&stationparameter_name=Level&returnfields=station_id,station_no,station_name,stationparameter_no,'
             'stationparameter_name,catchment_id,catchment_no,catchment_name,station_latitude,station_longitude,'
             'station_carteasting,station_cartnorthing,river_id,river_name,ca_sta&'
             'ca_sta_returnfields=CATCHMENT_SIZE,GAUGE_DATUM,GROUND_DATUM,GWREF_DATUM&object_type=General&format=csv')
-
-        self.__streams = src.functions.streams.Streams()
 
     def exc(self) -> pd.DataFrame:
         """
