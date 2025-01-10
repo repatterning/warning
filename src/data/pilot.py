88bb9aa1efb00d57a40d89ab8e374965b9c17bf4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-10 00:53:41 +0000

    Update src/data/pilot.py

--- src/data/pilot.py
+++ src/data/pilot.py
@@ -1,8 +1,12 @@
+"""Module pilot.py"""
 import logging
 import pandas as pd
 
 
 class Pilot:
+    """
+
+    """
 
     def __init__(self, assets: pd.DataFrame):
         """
@@ -12,17 +16,29 @@ class Pilot:
 
         self.__assets = assets
 
+    def __catchments(self):
+
+        numbers: pd.Series = self.__assets[['catchment_id', 'catchment_name']].groupby(
+            by=['catchment_id', 'catchment_name']).value_counts()
+
+        catchments = numbers.to_frame().reset_index()
+
+        return catchments.copy().sort_values(by='count', ascending=True)
+
+    def __slice(self):
+        pass
+
+
+
     def exc(self):
         """
 
         :return:
         """
 
-        catchments: pd.Series = self.__assets[['catchment_id', 'catchment_name']].groupby(
-            by=['catchment_id', 'catchment_name']).value_counts()
+        catchments = self.__catchments()
+        logging.info(catchments)
 
-        latest = catchments.to_frame().reset_index()
-
-        latest = latest.copy().sort_values(by='count', ascending=True)
-
-        logging.info(latest)
+        conditionals = (catchments['count'] >= 5) & (catchments['count'] <=7)
+        catchments = catchments.copy().loc[conditionals, :]
+        logging.info(catchments)
