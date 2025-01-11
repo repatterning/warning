a40f0570a30dc2161bbcb0f9618c0fcfe3cd3a3d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-11 17:26:20 +0000

    Update src/elements/partitions.py

--- src/elements/partitions.py
+++ src/elements/partitions.py
@@ -9,15 +9,19 @@ class Partitions(typing.NamedTuple):
     Attributes<br>
     ----------<br>
     <b>ts_id</b>: int<br>
-        The identification code of a time series.<br>
+        The identification code of a time series.<br><br>
     <b>datestr</b>: str<br>
-        The <b>date string of the start date of a period</b>, format %Y-%m-%d,  i.e., YYYY-mm-dd.<br>
+        The <b>date string of the start date of a period</b>, format %Y-%m-%d,  i.e., YYYY-mm-dd.<br><br>
     <b>catchment_size</b>: float<br>
-        The size of the catchment the time-series-measuring-station belongs to.<br>
+        The size of the catchment the time-series-measuring-station belongs to.<br><br>
     <b>gauge_datum</b>: float<br>
-        The elevation above ...<br>
+        References:
+            <a href="https://timeseriesdoc.sepa.org.uk/api-documentation/before-you-start/what-data-are-available/#Level"
+              target="_blank">River Level (Stage)</a>,
+            <a href="https://www.sepa.org.uk/environment/water/water-levels/"  target="_blank">Water Levels</a>,
+            <a href="https://waterdata.usgs.gov/blog/gage_height/" target="_blank">United States Geological Survey</a><br><br>
     <b>on_river</b>: bool<br>
-        An on river measuring station?
+        An on-river measuring station?<br>
     """
 
     ts_id: int
