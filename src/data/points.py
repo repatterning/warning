99854da006659b058b85271e461a74aeb1a32a25
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-11 18:10:09 +0000

    Update src/data/points.py

--- src/data/points.py
+++ src/data/points.py
@@ -1,6 +1,8 @@
 """Module points.py"""
 import logging
 
+import pandas as pd
+
 import config
 import src.elements.partitions as prt
 import src.functions.objects
@@ -20,15 +22,50 @@ class Points:
         """
 
         self.__configurations = config.Config()
-
         self.__objects = src.functions.objects.Objects()
 
+        # The uniform resource locator, data columns, etc.
         self.__url = ('https://timeseries.sepa.org.uk/KiWIS/KiWIS?service=kisters&type=queryServices&datasource=0'
                       '&request=getTimeseriesValues&ts_id={ts_id}'
                       f'&period={self.__configurations.period}'
                       '&from={datestr}&returnfields=Timestamp,Value,Quality Code&metadata=true'
                       '&md_returnfields=ts_id,ts_name,ts_unitname,ts_unitsymbol,station_id,'
                       'catchment_id,parametertype_id,parametertype_name,river_name&dateformat=UNIX&format=json')
+
+        self.__rename = {'Timestamp': 'timestamp', 'Value': 'value', 'Quality Code': 'quality_code'}
+
+    def __get_data(self, url: str):
+        """
+
+        :param url:
+        :return:
+        """
+
+        parts = self.__objects.api(url=url)
+
+        # The data in data frame form
+        columns = parts[0]['columns'].split(',')
+        frame = pd.DataFrame.from_records(data=parts[0]['data'], columns=columns)
+        frame.rename(columns=self.__rename, inplace=True)
+
+        # The identification codes of the time series
+        frame = frame.assign(
+            station_id=parts[0]['station_id'], catchment_id=parts[0]['catchment_id'])
+
+        return frame
+
+    def __extra_features(self, data: pd.DataFrame, partition: prt.Partitions):
+        """
+
+        :param data:
+        :param partition:
+        :return:
+        """
+
+        data = data.assign(
+            catchment_size=partition.catchment_size, gauge_datum=partition.gauge_datum, on_river=int(partition.on_river))
+
+        return data
 
     def exc(self, partitions: list[prt.Partitions]):
         """
@@ -40,6 +77,5 @@ class Points:
         for partition in partitions:
 
             url = self.__url.format(ts_id=partition.ts_id, datestr=partition.datestr)
-            logging.info(url)
-            # blob = self.__objects.api(url=url)
-            # logging.info(blob[0])
+            data = self.__get_data(url=url)
+            data = self.__extra_features(data=data.copy(), partition=partition)
