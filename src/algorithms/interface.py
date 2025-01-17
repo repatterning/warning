3fd535f6376d33ab848904f3fa38dce74619fab2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-17 20:36:39 +0000

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -7,10 +7,16 @@ import src.algorithms.points
 class Interface:
 
     def __init__(self):
-        pass
+
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+
+        self.__logger = logging.getLogger(__name__)
 
     def exc(self):
 
         points = src.algorithms.points.Points().exc()
-        logging.info(points.head())
         points.info()
+        self.__logger.info(points.head())
+
