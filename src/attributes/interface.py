a3dfb52742eb2a933a43f815c3f7acb52bc8cbd0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-18 23:58:12 +0000

    Update src/attributes/interface.py

--- src/attributes/interface.py
+++ src/attributes/interface.py
@@ -1,13 +1,22 @@
+"""Module interface.py"""
 import src.attributes.correlation
 import src.attributes.listings
 
 
 class Interface:
+    """
+    For steps investigating series features.
+    """
 
     def __init__(self):
         pass
 
-    def exc(self):
+    @staticmethod
+    def exc():
+        """
+
+        :return:
+        """
 
         listings = src.attributes.listings.Listings().exc()
         src.attributes.correlation.Correlation().exc(listings=listings)
