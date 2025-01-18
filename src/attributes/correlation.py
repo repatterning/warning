2edb226959eb18f245a1067df75c2dd1fd82cee6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-18 23:56:01 +0000

    Update src/attributes/correlation.py

--- src/attributes/correlation.py
+++ src/attributes/correlation.py
@@ -1,9 +1,21 @@
 
 import statsmodels.graphics.tsaplots as sgt
 
+import src.elements.text_attributes as txa
+import src.functions.streams
+
+
 class Correlation:
 
     def __init__(self):
-        pass
 
-    
+        self.__streams = src.functions.streams.Streams()
+
+    def exc(self, listings: list):
+
+        for listing in listings[:2]:
+
+            text = txa.TextAttributes(uri=listing, header=0)
+            data = self.__streams.read(text=text)
+
+            sgt.plot_pacf(x=data['value'].values)
