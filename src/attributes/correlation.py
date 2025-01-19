67107c85d9da828fab8d48feb949870c0080fa73
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-19 01:43:55 +0000

    Update src/attributes/correlation.py

--- src/attributes/correlation.py
+++ src/attributes/correlation.py
@@ -1,4 +1,5 @@
 import os
+import matplotlib.pyplot as plt
 
 import dask.dataframe
 import pandas as pd
@@ -10,11 +11,27 @@ class Correlation:
     def __init__(self):
         pass
 
-    def exc(self, listings: list):
+    @staticmethod
+    def exc(listings: list):
+        """
+
+        :param listings:
+        :return:
+        """
 
         for listing in listings[:2]:
 
-            data: pd.DataFrame = dask.dataframe.read_csv(os.path.join(listing, '*.csv')).compute()
+            try:
+                data: pd.DataFrame = dask.dataframe.read_csv(os.path.join(listing, '*.csv')).compute()
+            except ImportError as err:
+                raise err from err
+
             data.sort_values(by='timestamp', ascending=True, inplace=True)
 
-            sgt.plot_pacf(x=data['value'].values)
+            fig, ax = plt.subplots(1, 1, figsize=(4.1, 2.3))
+
+            sgt.plot_pacf(x=data['value'].values, ax=ax)
+
+            plt.show()
+
+
