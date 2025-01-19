22dc0f54aafca3895fc97120738657b09a8b61ea
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-19 00:43:49 +0000

    Update src/attributes/correlation.py

--- src/attributes/correlation.py
+++ src/attributes/correlation.py
@@ -1,21 +1,20 @@
+import os
 
+import dask.dataframe
+import pandas as pd
 import statsmodels.graphics.tsaplots as sgt
-
-import src.elements.text_attributes as txa
-import src.functions.streams
 
 
 class Correlation:
 
     def __init__(self):
-
-        self.__streams = src.functions.streams.Streams()
+        pass
 
     def exc(self, listings: list):
 
         for listing in listings[:2]:
 
-            text = txa.TextAttributes(uri=listing, header=0)
-            data = self.__streams.read(text=text)
+            data: pd.DataFrame = dask.dataframe.read_csv(os.path.join(listing, '*.csv')).compute()
+            data.sort_values(by='timestamp', ascending=True, inplace=True)
 
             sgt.plot_pacf(x=data['value'].values)
