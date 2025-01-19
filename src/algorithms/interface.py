64d55428815813aaa53b932668a7b0afa1f278f1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-19 18:27:03 +0000

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -5,6 +5,7 @@ import time
 
 import pandas as pd
 
+import config
 import src.algorithms.algorithm
 import src.algorithms.points
 
@@ -19,8 +20,12 @@ class Interface:
         Constructor
         """
 
+        self.__configurations = config.Config()
+
+        # The variables
         self.__columns = ['value', 'catchment_size', 'gauge_datum', 'on_river']
 
+        # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
                             datefmt='%Y-%m-%d %H:%M:%S')
@@ -36,9 +41,8 @@ class Interface:
         points: pd.DataFrame = src.algorithms.points.Points().exc()
         points.info()
 
-        cutoff = datetime.datetime.strptime('2025-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
-        limit = 1000 * time.mktime(cutoff.timetuple())
-        frame: pd.DataFrame = points.copy().loc[points['timestamp'] < limit, :]
+        frame: pd.DataFrame = points.copy().loc[points['timestamp'] < self.__configurations.cutoff, :]
         frame.sort_values(by=['station_id', 'timestamp'], ascending=True, inplace=True)
+        frame.info()
 
-        src.algorithms.algorithm.Algorithm().exc(n_lags=2, frame=points, columns=self.__columns, groupings='station_id', _priors=False)
+        src.algorithms.algorithm.Algorithm().exc(n_lags=2, frame=frame, columns=self.__columns, groupings='station_id', _priors=False)
