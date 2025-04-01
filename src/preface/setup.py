01f06fffad69a4c4079587a4653a9512d0a4b4aa
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-01 18:52:58 +0100

    Update src/preface/setup.py

--- src/preface/setup.py
+++ src/preface/setup.py
@@ -1,12 +1,6 @@
 """Module setup.py"""
-
 import config
-import src.elements.s3_parameters as s3p
-import src.elements.service as sr
 import src.functions.directories
-import src.s3.bucket
-import src.s3.keys
-import src.s3.prefix
 
 
 class Setup:
@@ -14,59 +8,26 @@ class Setup:
     Description
     -----------
 
-    Sets up local & cloud environments
+    Sets up local environments
     """
 
-    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
+    def __init__(self):
+        """
+        Constructor
         """
 
-        :param service: A suite of services for interacting with Amazon Web Services.
-        :param s3_parameters: The overarching S3 (Simple Storage Service) parameters
-                              settings of this project, e.g., region code name, buckets, etc.
-        """
-
-        self.__service: sr.Service = service
-        self.__s3_parameters: s3p.S3Parameters = s3_parameters
         self.__configurations = config.Config()
+        self.__directories = src.functions.directories.Directories()
 
-        # The prefix in focus within the Amazon S3 bucket in focus.
-        self.__prefix = '' + '/'
-
-    def __clear_prefix(self) -> bool:
+    def __data(self) -> bool:
         """
 
         :return:
         """
 
-        # An instance for interacting with objects within an Amazon S3 prefix
-        instance = src.s3.prefix.Prefix(service=self.__service, bucket_name=self.__s3_parameters.internal)
+        self.__directories.cleanup(path=self.__configurations.data_)
 
-        # Get the keys therein
-        keys: list[str] = instance.objects(prefix=self.__prefix)
-
-        if len(keys) > 0:
-            objects = [{'Key' : key} for key in keys]
-            state = instance.delete(objects=objects)
-            return bool(state)
-
-        return True
-
-    def __s3(self) -> bool:
-        """
-        Prepares the relevant path within an Amazon S3 (Simple Storage Service) bucket.
-
-        :return:
-        """
-
-        # An instance for interacting with Amazon S3 buckets.
-        bucket = src.s3.bucket.Bucket(service=self.__service, location_constraint=self.__s3_parameters.location_constraint,
-                                      bucket_name=self.__s3_parameters.internal)
-
-        # If the bucket exist, the prefix path is cleared.  Otherwise, the bucket is created.
-        if bucket.exists():
-            return True
-
-        return bucket.create()
+        return self.__directories.create(path=self.__configurations.data_)
 
     def __local(self) -> bool:
         """
@@ -74,10 +35,10 @@ class Setup:
         :return:
         """
 
-        directories = src.functions.directories.Directories()
-        directories.cleanup(path=self.__configurations.warehouse)
 
-        return directories.create(path=self.__configurations.series_)
+        self.__directories.cleanup(path=self.__configurations.warehouse)
+
+        return self.__directories.create(path=self.__configurations.warehouse)
 
     def exc(self) -> bool:
         """
@@ -85,4 +46,4 @@ class Setup:
         :return:
         """
 
-        return self.__s3() & self.__local()
+        return self.__data() & self.__local()
