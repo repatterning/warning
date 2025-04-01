4507c1832a56fd10dcdc87778813011533e2b3d5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-01 21:10:48 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 import logging
 import os
 import sys
+import boto3
 
 
 def main():
@@ -13,6 +14,9 @@ def main():
 
     logger: logging.Logger = logging.getLogger(__name__)
     logger.info('EVENTS')
+
+    # Assets
+    src.assets.Assets(s3_parameters=s3_parameters).exc()
 
     # Delete Cache Points
     src.functions.cache.Cache().exc()
@@ -32,6 +36,15 @@ if __name__ == '__main__':
 
     # Modules
     import src.functions.cache
+    import src.preface.interface
+    import src.elements.service as sr
+    import src.elements.s3_parameters as s3p
+    import src.assets
 
-    
+    connector: boto3.session.Session
+    s3_parameters: s3p
+    service: sr.Service
+    arguments: dict
+    connector, s3_parameters, service, arguments = src.preface.interface.Interface().exc()
+
     main()
