8460e404808bd95951c52d1853816faaed890f1b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-01 22:23:53 +0100

    Update src/data/interface.py: In Progress

--- src/data/interface.py
+++ src/data/interface.py
@@ -1,14 +1,25 @@
 """Module interface.py"""
 import logging
+import os
 import src.data.codes
+import src.data.reference
+import src.elements.s3_parameters as s3p
 
 class Interface:
     """
     Interface
     """
 
-    def __init__(self):
+    def __init__(self, s3_parameters: s3p.S3Parameters):
+        """
 
+        :param s3_parameters: The overarching S3 (Simple Storage Service) parameters
+                              settings of this project, e.g., region code name, buckets, etc.
+        """
+
+        self.__s3_parameters: s3p.S3Parameters = s3_parameters
+
+        # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
                             datefmt='%Y-%m-%d %H:%M:%S')
@@ -18,3 +29,8 @@ class Interface:
 
         codes: list[str] = src.data.codes.Codes().exc()
         self.__logger.info(codes)
+
+        numeric = [int(os.path.split(code)[-1]) for code in codes]
+        logging.info(numeric)
+
+        # src.data.reference.Reference(s3_parameters=self.__s3_parameters).exc()
