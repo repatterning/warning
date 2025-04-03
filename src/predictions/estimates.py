77c907a5e4e8224c51c8704b4e47324f5786d7ee
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-03 14:39:35 +0100

    Update src/predictions/estimates.py

--- src/predictions/estimates.py
+++ src/predictions/estimates.py
@@ -1,9 +1,14 @@
 """Module boundaries.py"""
+import logging
+import json
+import os
 import numpy as np
 import pandas as pd
-import scipy.stats as sta
 
+import config
 import src.elements.parts as pr
+import src.elements.specifications as se
+import src.functions.objects
 
 
 class Estimates:
@@ -17,6 +22,11 @@ class Estimates:
         """
         Constructor
         """
+
+        self.__configurations = config.Config()
+        self.__path = os.path.join(self.__configurations.points_, 'predictions')
+
+        self.__objects = src.functions.objects.Objects()
 
     @staticmethod
     def __get_errors(data: pd.DataFrame) -> pd.DataFrame:
@@ -32,17 +42,50 @@ class Estimates:
 
         return data
 
-    def exc(self, parts: pr.Parts) -> pr.Parts:
+    @staticmethod
+    def __get_node(blob: pd.DataFrame) -> dict:
+        """
+
+        :param blob:
+        :return:
+        """
+
+        string: str = blob.to_json(orient='split')
+
+        return json.loads(string)
+
+    def __persist(self, nodes: dict, name: str) -> None:
+        """
+
+        :param nodes:
+        :param name:
+        :return:
+        """
+
+        message =  self.__objects.write(
+            nodes=nodes, path=os.path.join(self.__path, f'{name}.json'))
+        logging.info(message)
+
+    def exc(self, parts: pr.Parts, specifications: se.Specifications) -> pr.Parts:
         """
 
         :param parts:
+        :param specifications:
         :return:
         """
 
         training = self.__get_errors(data=parts.training.copy())
         testing = self.__get_errors(data=parts.testing.copy())
-
-
         parts = parts._replace(training=training, testing=testing)
+
+        nodes = {
+            'training': self.__get_node(parts.training),
+            'testing': self.__get_node(parts.testing),
+            'futures': parts.futures.to_dict(orient='split')}
+
+        spe = specifications._asdict()
+        for value in ['station_id', 'catchment_id', 'ts_id']:
+            spe[value] = int(spe.get(value))
+        nodes.update(spe)
 
         return parts
