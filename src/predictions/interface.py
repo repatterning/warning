40364f556dc3b25971b37bcd1564fc1d60a5f425
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-03 14:39:48 +0100

    Update src/predictions/interface.py

--- src/predictions/interface.py
+++ src/predictions/interface.py
@@ -1,27 +1,57 @@
 
+import logging
 import dask
+import os
 
+import config
 import src.elements.specifications as se
 import src.elements.parts as pr
 import src.predictions.data
 import src.predictions.estimates
+import src.predictions.metrics
+import src.functions.directories
 
 
 
 class Interface:
 
     def __init__(self):
-        pass
+
+        self.__configurations = config.Config()
+
+    def __directories(self):
+        """
+
+        :return:
+        """
+
+        directories = src.functions.directories.Directories()
+
+        for section in ['predictions', 'errors']:
+            path = os.path.join(self.__configurations.points_, section)
+            directories.create(path)
 
 
     def exc(self, specifications_: list[se.Specifications]):
+        """
+
+        :param specifications_:
+        :return:
+        """
+
+        self.__directories()
 
         __get_data = dask.delayed(src.predictions.data.Data().exc)
-        __get_errors = dask.delayed(src.predictions.estimates.Estimates().exc)
-
+        __get_estimates = dask.delayed(src.predictions.estimates.Estimates().exc)
+        __get_metrics = dask.delayed(src.predictions.metrics.Metrics().exc)
 
         computations = []
         for specifications in specifications_:
 
             parts: pr.Parts = __get_data(specifications=specifications)
-            errors: pr.Parts = __get_errors(parts=parts)
+            parts_ = __get_estimates(parts=parts, specifications=specifications)
+            message_m = __get_metrics(parts=parts_, specifications=specifications)
+            computations.append(message_m)
+
+        messages = dask.compute(computations, scheduler='threads', num_workers=8)[0]
+        logging.info(messages)
