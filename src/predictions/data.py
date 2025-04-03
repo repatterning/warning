525f84081d1a238450c8dd14bf709a90a328082f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-04-03 14:39:25 +0100

    Update src/predictions/data.py

--- src/predictions/data.py
+++ src/predictions/data.py
@@ -36,19 +36,9 @@ class Data:
         """
 
         frame = pd.DataFrame.from_dict(data[section], orient='tight')
-        frame.rename(columns={'date': 'week_ending_date'}, inplace=True)
-
-        # ascertain datetime dates
-        frame['week_ending_date'] = pd.to_datetime(
-            frame['week_ending_date'].astype(str), errors='coerce', format='%Y-%m-%d')
-
-        # the milliseconds epoch for graphing
-        frame['milliseconds']  = (
-                frame['week_ending_date'].to_numpy().astype(np.int64) / (10 ** 6)
-        ).astype(np.longlong)
 
         # sort
-        frame.sort_values(by='week_ending_date', ascending=True, inplace=True)
+        frame.sort_values(by='timestamp', ascending=True, inplace=True)
 
         return frame
 
