f3642920b13a8ade3b4c7523e1d71d37ee3cab66
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-19 13:31:15 +0100

    Update src/data/codes.py

--- src/data/codes.py
+++ src/data/codes.py
@@ -10,7 +10,7 @@ class Codes:
     """
     <b>Notes</b><br>
     ------<br>
-    Determines the institutions list.<br>
+    Determines the applicable list of gauge time series codes.<br>
     """
 
     def __init__(self):
@@ -19,21 +19,6 @@ class Codes:
         """
 
         self.__configurations = config.Config()
-        self.__path_m = os.path.join(self.__configurations.data_, 'models')
-        self.__path_d = os.path.join(self.__configurations.data_, 'data')
-
-    @staticmethod
-    def __re_cut(string: str) -> str:
-        """
-
-        :param string: A path string
-        :return:
-        """
-
-        string = string.rstrip(os.sep)
-        values = string.split(sep=os.sep)
-
-        return values[-2] + os.sep + values[-1]
 
     def __get_codes(self) -> list[str] | None:
         """
@@ -41,17 +26,16 @@ class Codes:
         :return:
         """
 
-        listings = glob.glob(pathname=os.path.join(self.__path_m, '**', '**'))
-        stems = [self.__re_cut(listing) for listing in listings]
+        listings = glob.glob(pathname=os.path.join(self.__configurations.data_, '**', '**'))
 
         codes = []
-        for stem in stems:
-            state = (pathlib.Path(os.path.join(self.__path_m, stem, 'estimates.json')).exists() &
-                     pathlib.Path(os.path.join(self.__path_d, stem, 'data.csv')).exists() &
-                     pathlib.Path(os.path.join(self.__path_d, stem, 'training.csv')).exists() &
-                     pathlib.Path(os.path.join(self.__path_d, stem, 'testing.csv')).exists())
+        for listing in listings:
+            state = (pathlib.Path(os.path.join(listing, 'estimates.json')).exists() &
+                     pathlib.Path(os.path.join(listing, 'data.csv')).exists() &
+                     pathlib.Path(os.path.join(listing, 'training.csv')).exists() &
+                     pathlib.Path(os.path.join(listing, 'testing.csv')).exists())
             if state:
-                codes.append(stem)
+                codes.append(os.path.basename(listing))
         return codes
 
     def exc(self) -> list[str] | None:
