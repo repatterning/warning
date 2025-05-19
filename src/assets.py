95a2d32b3dfd2c89cd015226c29665a7bb39e302
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-19 13:18:34 +0100

    Update src/assets.py

--- src/assets.py
+++ src/assets.py
@@ -4,11 +4,15 @@ import logging
 import os
 import sys
 
+import numpy as np
+
 import config
 import src.elements.s3_parameters as s3p
+import src.elements.service as sr
 import src.functions.cache
 import src.s3.directives
 import src.s3.unload
+import src.s3.keys
 
 
 class Assets:
@@ -20,32 +24,51 @@ class Assets:
     will be unnecessary, edit accordingly.</b>
     """
 
-    def __init__(self, s3_parameters: s3p.S3Parameters):
+    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
         """
 
+        :param service:
         :param s3_parameters: The overarching S3 (Simple Storage Service) parameters
                               settings of this project, e.g., region code name, buckets, etc.
         """
 
+        self.__service = service
         self.__s3_parameters: s3p.S3Parameters = s3_parameters
 
         # Setting up
         self.__configurations = config.Config()
         self.__source_bucket = self.__s3_parameters.internal
-        self.__prefix = self.__s3_parameters.path_internal_artefacts + self.__configurations.stamp
 
         # Directives
         self.__directives = src.s3.directives.Directives()
 
-    def __get_assets(self) -> int:
+    def __get_origin(self) -> str:
         """
 
         :return:
         """
 
+        elements = src.s3.keys.Keys(
+            service=self.__service, bucket_name=self.__s3_parameters.internal).excerpt(prefix='assets/autoregressive/')
+        keys = [element.split('/', maxsplit=3)[2] for element in elements]
+        strings = list(set(keys))
+        values = np.array(strings, dtype='datetime64')
+        stamp = str(values.max())
+
+        logging.info(values)
+
+        return self.__configurations.origin_.format(stamp=stamp)
+
+    def __get_assets(self, origin: str) -> int:
+        """
+
+        :param origin:
+        :return:
+        """
+
         try:
             return self.__directives.synchronise(
-                source_bucket=self.__source_bucket, origin=self.__prefix, target=self.__configurations.data_)
+                source_bucket=self.__source_bucket, origin=origin, target=self.__configurations.data_)
         except RuntimeError as err:
             raise err from err
 
@@ -55,8 +78,10 @@ class Assets:
         :return:
         """
 
+        origin = self.__get_origin()
+
         # The artefacts, vis-à-vis modelling.
-        state = self.__get_assets()
+        state = self.__get_assets(origin=origin)
         logging.info('Assets State: %s', state)
 
         # Third Eye
