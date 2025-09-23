9c95427338ec401b3f0340778101097ec115f0ca
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-23 20:25:51 +0100

    Update src/compute/schedule.py

--- src/compute/schedule.py
+++ src/compute/schedule.py
@@ -10,41 +10,42 @@ class Schedule:
     Schedule
     """
 
-    def __init__(self):
+    def __init__(self, connector: boto3.session.Session):
         """
         Constructor
         """
 
-        self.__scheduler_client = boto3.client('scheduler')
+        # self.__scheduler_client = boto3.client('scheduler')
+        self.__scheduler_client = connector.client(service_name='scheduler')
 
         # Logging
         logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def create_schedule(self, arguments: dict) -> str:
+    def create_schedule(self, settings: dict) -> str:
         """
         Creates a new schedule with the specified parameters.
 
-        :param arguments:
+        :param settings:
         :return:
             The ARN of the created schedule.
         """
         try:
             parameters = {
-                'Name': arguments.get('name'),
-                'ScheduleExpression': arguments.get('schedule_expression'),
-                'GroupName': arguments.get('group_name'),
-                'Target': {'Arn': arguments.get('arn'), 'RoleArn': arguments.get('role_arn')},
-                'StartDate': arguments.get('starting'),
-                'EndDate': arguments.get('ending')}
+                'Name': settings.get('name'),
+                'ScheduleExpression': settings.get('schedule_expression'),
+                'GroupName': settings.get('group_name'),
+                'Target': {'Arn': settings.get('arn'), 'RoleArn': settings.get('role_arn')},
+                'StartDate': settings.get('starting'),
+                'EndDate': settings.get('ending')}
 
-            if arguments.get('delete_after_completion'):
+            if settings.get('delete_after_completion'):
                 parameters['ActionAfterCompletion'] = 'DELETE'
 
-            if arguments.get('use_flexible_window_time'):
+            if settings.get('use_flexible_window_time'):
                 parameters['FlexibleTimeWindow']['Mode'] = 'FLEXIBLE'
-                parameters['FlexibleTimeWindow']['MaximumWindowInMinutes'] = arguments.get('maximum_window_in_minutes')
+                parameters['FlexibleTimeWindow']['MaximumWindowInMinutes'] = settings.get('maximum_window_in_minutes')
             else:
                 parameters['FlexibleTimeWindow']['Mode'] = 'OFF'
 
@@ -54,7 +55,7 @@ class Schedule:
 
         except botocore.exceptions.ClientError as err:
             if err.response['Error']['Code'] == 'ConflictException':
-                self.__logger.error('Failed to create schedule %s: %s', arguments.get('name'), err.response['Error']['Message'])
+                self.__logger.error('Failed to create schedule %s: %s', settings.get('name'), err.response['Error']['Message'])
             else:
                 self.__logger.error('Error creating schedule: %s', err.response['Error']['Message'])
             raise
