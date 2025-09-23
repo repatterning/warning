bb7ec35e9bfb11f6c0d5a018862bf3c88d89b8ec
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-23 17:07:23 +0100

    Update src/cartography/interface.py: In Progress

--- src/cartography/interface.py
+++ src/cartography/interface.py
@@ -9,8 +9,10 @@ import pandas as pd
 import src.cartography.cuttings
 import src.cartography.data
 import src.cartography.reference
+import src.cartography.updating
 import src.elements.s3_parameters as s3p
 import src.elements.system as stm
+import src.elements.service as sr
 import src.functions.cache
 import src.functions.streams
 
@@ -20,9 +22,10 @@ class Interface:
     The interface to the programs of the algorithms package.
     """
 
-    def __init__(self, connector: boto3.session.Session, s3_parameters: s3p.S3Parameters, arguments: dict):
+    def __init__(self, service: sr.Service, connector: boto3.session.Session, s3_parameters: s3p.S3Parameters, arguments: dict):
         """
 
+        :param service:
         :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
                           Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
         :param s3_parameters: The overarching S3 parameters settings of this project, e.g., region code
@@ -30,6 +33,7 @@ class Interface:
         :param arguments: A set of arguments vis-à-vis computation & data operations objectives.
         """
 
+        self.__service = service
         self.__connector = connector
         self.__s3_parameters = s3_parameters
         self.__arguments = arguments
@@ -38,10 +42,6 @@ class Interface:
     def __members(data: geopandas.GeoDataFrame, reference: geopandas.GeoDataFrame) -> geopandas.GeoDataFrame:
         """
         Which gauges, if any, lie within a warning area?
-
-        initial: list[geopandas.GeoDataFrame] = [
-            src.cartography.cuttings.Cuttings(reference=reference).members(_polygon=_polygon)
-            for _polygon in data.geometry]
 
         :param data:
         :param reference:
@@ -73,9 +73,12 @@ class Interface:
         data: geopandas.GeoDataFrame = src.cartography.data.Data(
             connector=self.__connector, arguments=self.__arguments).exc()
         data: geopandas.GeoDataFrame = data.to_crs(epsg=int(reference.crs.srs.split(':')[1]))
-        data.info()
 
         # Hence
         frame: geopandas.GeoDataFrame = self.__members(data=data, reference=reference)
-        frame.info()
-        logging.info(frame)
+
+        # Update the warnings data library
+        message = src.cartography.updating.Updating(
+            service=self.__service, s3_parameters=self.__s3_parameters).exc(frame=frame)
+
+
