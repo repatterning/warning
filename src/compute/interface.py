f899b30ee05407bc6b84d10aad8236cab83c4805
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-23 21:40:16 +0100

    Update src/compute/interface.py

--- src/compute/interface.py
+++ src/compute/interface.py
@@ -1,23 +1,37 @@
-import src.compute.settings
-import src.compute.schedule
+"""Module compute/interface.py"""
+import logging
 import boto3
+
+import src.compute.schedule
+import src.compute.settings
 
 
 class Interface:
+    """
+    The interface to the compute package programs
+    """
 
     def __init__(self, connector: boto3.session.Session, arguments: dict):
         """
 
-        :param connector:
-        :param arguments:
+        :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
+                          Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
+        :param arguments: A set of arguments vis-à-vis computation & data operations objectives.
         """
 
         self.__connector = connector
         self.__arguments = arguments
 
-
-
     def exc(self, starting, ending):
+        """
+
+        :param starting:
+        :param ending:
+        :return:
+        """
+
+        logging.info(type(starting))
+        logging.info(type(ending))
 
         settings = src.compute.settings.Settings(
             connector=self.__connector, project_key_name=self.__arguments.get('project_key_name')).exc(
