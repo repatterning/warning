90c009d66343437d294bdaf54cff979edb09ee81
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-23 12:43:38 +0100

    Add src/elements/system.py

---
+++ src/elements/system.py
@@ -0,0 +1,30 @@
+import typing
+import numpy as np
+import shapely
+
+import datetime
+
+class System(typing.NamedTuple):
+    """
+    The data type class ⇾ System
+
+    Attributes
+    ----------
+
+    """
+
+    Index: int
+    issuedDate: datetime.datetime
+    warningLikelihood: np.int32
+    warningLevel: str
+    warningStatus: str
+    warningHeadline: str
+    warningId: str
+    warningVersion: str
+    warningFurtherDetails: str
+    modifiedDate: datetime.datetime
+    validFromDate: datetime.datetime
+    affectedAreas: str
+    warningImpact: np.int32
+    validToDate: datetime.datetime
+    geometry: shapely.geometry.polygon.Polygon
