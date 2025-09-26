d465d14d893d68060c6116d32e49d611c2ab0352
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-26 17:39:36 +0100

    Update src/transfer/metadata.py

--- src/transfer/metadata.py
+++ src/transfer/metadata.py
@@ -12,14 +12,19 @@ class Metadata:
         """
 
         self.metadata = {
-            'ts_id': 'The unique identification of the time series of a gauge.',
-            'catchment_id': 'A catchment identification code.',
-            'latitude': 'Latitude',
-            'longitude': 'Longitude',
-            'issued_date': 'The day the warning was issued',
-            'warning_level': 'The warning level.',
-            'warning_id': 'The identification code of the warning.',
-            'modified': 'If the warning modified, this value will differ from the issue date.',
-            'starting': "The warning period's starting time point.",
-            'ending': "The warning period's ending time point"
+            "latest.geojson": {
+                "desc": "The latest geospatial warning data."
+            },
+            "data.csv": {
+                'ts_id': 'The unique identification of the time series of a gauge.',
+                 'catchment_id': 'A catchment identification code.',
+                 'latitude': 'Latitude',
+                 'longitude': 'Longitude',
+                 'issued_date': 'The day the warning was issued',
+                 'warning_level': 'The warning level.',
+                 'warning_id': 'The identification code of the warning.',
+                 'modified': 'If the warning modified, this value will differ from the issue date.',
+                 'starting': "The warning period's starting time point.",
+                 'ending': "The warning period's ending time point"
+            }
         }
