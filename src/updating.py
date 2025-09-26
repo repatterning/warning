75dd57ea0fd87ca65e1cc51fbd03937b7ccdeec6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-26 15:21:20 +0100

    Update src/updating.py

--- src/updating.py
+++ src/updating.py
@@ -1,13 +1,13 @@
 """Module data/updating.py"""
+import os
+
 import pandas as pd
 
-import src.cartography.metadata
+import config
 import src.elements.s3_parameters as s3p
-import src.elements.service as sr
 import src.elements.text_attributes as txa
 import src.functions.cache
 import src.functions.streams
-import src.s3.upload
 
 
 class Updating:
@@ -15,27 +15,24 @@ class Updating:
     Updating
     """
 
-    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters):
+    def __init__(self, s3_parameters: s3p.S3Parameters):
         """
 
-        :param service: A suite of services for interacting with Amazon Web Services.
         :param s3_parameters: The overarching S3 parameters settings of this project, e.g., region code
                               name, buckets, etc.
         """
 
-        self.__service = service
         self.__s3_parameters = s3_parameters
 
         # Instances
+        self.__configurations = config.Config()
         self.__streams = src.functions.streams.Streams()
-        self.__metadata = src.cartography.metadata.Metadata()
 
-    def __update(self, uri: str, frame: pd.DataFrame, affix: str) -> bool:
+    def __update(self, uri: str, frame: pd.DataFrame) -> str:
         """
 
         :param uri: Storage path.
         :param frame: Warning data
-        :param affix:
         :return:
         """
 
@@ -45,11 +42,10 @@ class Updating:
         instances = pd.concat([original, frame], axis=0, ignore_index=True)
         instances.drop_duplicates(inplace=True)
 
-        return src.s3.upload.Upload(
-            service=self.__service, s3_parameters=self.__s3_parameters).bytes(
-            data=instances, metadata=self.__metadata.metadata, key_name=affix)
+        return self.__streams.write(
+            blob=instances, path=os.path.join(self.__configurations.warehouse, self.__configurations.affix.replace('/', os.sep)))
 
-    def exc(self, frame: pd.DataFrame) -> bool:
+    def exc(self, frame: pd.DataFrame) -> str:
         """
 
         :param frame: The data of a gauge.
@@ -57,7 +53,6 @@ class Updating:
         """
 
         frame.drop(columns='geometry', inplace=True)
-        affix = 'warning/data.csv'
-        uri = f's3://{self.__s3_parameters.internal}/{affix}'
+        uri = f's3://{self.__s3_parameters.internal}/{self.__configurations.affix}'
 
-        return self.__update(uri=uri, frame=frame, affix=affix)
+        return self.__update(uri=uri, frame=frame)
