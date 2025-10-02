23ddce21f074e07bfe5c7280f46f4f4234662fb3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-02 22:44:20 +0100

    Update src/compute/timings.py: In Progress

--- src/compute/timings.py
+++ src/compute/timings.py
@@ -1,6 +1,45 @@
-
+import logging
+import datetime
 
 class Timings:
 
-    def __init__(self):
-        pass
+    def __init__(self, arguments: dict, starting: datetime.datetime, ending: datetime.datetime):
+
+        self.__arguments = arguments
+        self.__starting = starting
+        self.__ending = ending
+
+    def __events_forecasting(self, focus: str):
+        """
+        Builds cron expressions, e.g., "cron(45 09,21 * * ? *)"
+
+        :return:
+        """
+
+        # The cron expression template
+        expression = "cron({minute} {initial},{later} * * ? *)"
+
+        # The start & in-between times
+        start = self.__starting + datetime.timedelta(minutes=25)
+        inbetween = start + datetime.timedelta(hours=12)
+
+
+        __scheduler = self.__arguments.get(focus)
+        __scheduler['schedule_expression'] = expression.format(
+            minute=start.minute, initial=start.hour, later=inbetween.hour)
+        __scheduler['starting'] = self.__starting
+        __scheduler['ending'] = self.__ending
+
+        return __scheduler
+
+    def exc(self, focus: str):
+
+        match focus:
+            case 'scheduler_events_forecasting':
+                return self.__events_forecasting(focus=focus)
+            case 'scheduler_events_fundamental':
+                logging.info(focus)
+            case 'scheduler_continuous':
+                logging.info(focus)
+            case _:
+                raise ValueError('{focus} is not an option.')
