b884cdaa6aafa5130c1a735e2a349f65680a632e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-02 17:32:34 +0100

    Update src/cartography/times.py: In Progress

--- src/cartography/times.py
+++ src/cartography/times.py
@@ -1,13 +1,23 @@
-
+import os
 import logging
 
 import geopandas
 
+import config
+import src.functions.objects
+
 
 class Times:
+    """
+    The start & end times vis-à-vis a weather warning period.
+    """
 
     def __init__(self):
+        """
+        Constructor
+        """
 
+        self.__configurations = config.Config()
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -17,13 +27,23 @@ class Times:
         self.__logger = logging.getLogger(__name__)
 
     def exc(self, data: geopandas.GeoDataFrame):
+        """
+
+        :param data:
+        :return:
+        """
 
         data.info()
         self.__logger.info(data)
         self.__logger.info(data['ending'].max())
         self.__logger.info(data['starting'].min())
 
-        dictionary = {
+        nodes = {
             'starting': data['starting'].min(),
             'ending': data['ending'].max()
         }
+
+        path = os.path.join(self.__configurations.warning_, 'times.json')
+
+        objects = src.functions.objects.Objects()
+        objects.write(nodes=nodes, path=path)
