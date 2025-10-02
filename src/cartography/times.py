038a91555eac2c14b2cad780ca0f3c5579f0b2c6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-02 17:19:57 +0100

    Update src/cartography/times.py: In Progress

--- src/cartography/times.py
+++ src/cartography/times.py
@@ -1,6 +1,29 @@
 
+import logging
+
+import geopandas
+
 
 class Times:
 
     def __init__(self):
-        pass
+
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+
+        self.__logger = logging.getLogger(__name__)
+
+    def exc(self, data: geopandas.GeoDataFrame):
+
+        data.info()
+        self.__logger.info(data)
+        self.__logger.info(data['ending'].max())
+        self.__logger.info(data['starting'].min())
+
+        dictionary = {
+            'starting': data['starting'].min(),
+            'ending': data['ending'].max()
+        }
