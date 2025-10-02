007f8e68ab74359ee4b1ab7bf81c4709619ed19e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-02 23:09:56 +0100

    Update src/compute/timings.py

--- src/compute/timings.py
+++ src/compute/timings.py
@@ -17,11 +17,11 @@ class Timings:
         self.__starting = starting
         self.__ending = ending
 
-    def __events_forecasting(self, focus: str) -> dict:
+    def __events_forecasting(self, scheduler: str) -> dict:
         """
         Builds cron expressions, e.g., "cron(45 09,21 * * ? *)"
 
-        :param focus:
+        :param scheduler:
         :return:
         """
 
@@ -33,7 +33,7 @@ class Timings:
         inbetween = start + datetime.timedelta(hours=12)
 
         # Scheduler
-        __scheduler = self.__arguments.get(focus)
+        __scheduler = self.__arguments.get(scheduler)
         __scheduler['schedule_expression'] = expression.format(
             minute=start.minute, initial=start.hour, later=inbetween.hour)
         __scheduler['starting'] = self.__starting
@@ -41,48 +41,48 @@ class Timings:
 
         return __scheduler
 
-    def __events_fundamental(self, focus: str) -> dict:
+    def __events_fundamental(self, scheduler: str) -> dict:
         """
 
-        :param focus:
+        :param scheduler:
         :return:
         """
 
-        __scheduler = self.__arguments.get(focus)
+        __scheduler = self.__arguments.get(scheduler)
         __scheduler['starting'] = self.__starting
         __scheduler['ending'] = self.__ending
 
         return __scheduler
 
-    def __continuous(self, focus: str) -> dict:
+    def __continuous(self, scheduler: str) -> dict:
         """
 
-        :param focus:
+        :param scheduler:
         :return:
         """
 
         __starting, __ending = src.compute.continuous.Continuous(
             arguments=self.__arguments).exc(ending=self.__ending)
 
-        __scheduler = self.__arguments.get(focus)
+        __scheduler = self.__arguments.get(scheduler)
         __scheduler['starting'] = __starting
         __scheduler['ending'] = __starting
 
         return __scheduler
 
-    def exc(self, focus: str) -> dict:
+    def exc(self, scheduler: str) -> dict:
         """
 
-        :param focus:
+        :param scheduler:
         :return:
         """
 
-        match focus:
+        match scheduler:
             case 'scheduler_events_forecasting':
-                return self.__events_forecasting(focus=focus)
+                return self.__events_forecasting(scheduler=scheduler)
             case 'scheduler_events_fundamental':
-                return self.__events_fundamental(focus=focus)
+                return self.__events_fundamental(scheduler=scheduler)
             case 'scheduler_continuous':
-                return self.__continuous(focus=focus)
+                return self.__continuous(scheduler=scheduler)
             case _:
-                raise ValueError(f'{focus} is not an option.')
+                raise ValueError(f'{scheduler} is not an option.')
