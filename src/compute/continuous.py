b69e0243ef90092113b6c8dcdb33346a1de20bd6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-02 21:30:31 +0100

    Update src/compute/continuous.py

---
+++ src/compute/continuous.py
@@ -0,0 +1,32 @@
+import datetime
+import typing
+
+
+class Continuous:
+
+    def __init__(self, arguments: dict):
+        """
+
+        :param arguments:
+        """
+
+        self.__terminate: dict = arguments.get('scheduler_continuous').get('terminate')
+
+    def exc(self, ending: datetime.datetime) -> typing.Tuple[datetime.datetime, datetime.datetime]:
+        """
+
+        :param ending: The time the warning period ends
+        :return:
+        """
+
+        # Start
+        __next = ending.date() + datetime.timedelta(days=1)
+        __starting = datetime.datetime(year=__next.year, month=__next.month, day=__next.day, hour=2, minute=5, second=0)
+
+        # End
+        __ending = datetime.datetime(
+            year=self.__terminate.get('year'),
+            month=self.__terminate.get('month'),
+            day=self.__terminate.get('day'))
+
+        return __starting, __ending
