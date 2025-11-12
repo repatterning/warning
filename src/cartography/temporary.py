18130b5dcdaa06aaeff0e9e7a52b6b0a6c7d0297
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-11-12 16:49:19 +0000

    Update src/cartography/temporary.py: In Progress

--- src/cartography/temporary.py
+++ src/cartography/temporary.py
@@ -1,5 +1,4 @@
 """Module temporary.py"""
-import logging
 import datetime
 import uuid
 
@@ -38,7 +37,7 @@ class Temporary:
             frame['warningId'] = str(uuid.uuid4())
             frame['validFromDate'] = baseline + datetime.timedelta(minutes=self.__minutes)
             frame['validToDate'] = baseline + datetime.timedelta(minutes=3*self.__minutes)
-            logging.info(frame)
+            frame.info()
             return frame
         except FileNotFoundError as err:
             raise err from err
